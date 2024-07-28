61b220110fa2aed2ddd1d3e3f5c3769aed99f856
omen <paulk198@hotmail.com>
2024-07-29 00:44:27 +0200

    first commit

---
+++ api/Controllers/CommentController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+[Route("api/comment")]
+public class CommentController : ControllerBase
+{
+    private readonly ICommentRepository _commentRepo;
+
+    public CommentController(ICommentRepository commentRepo)
+    {
+        _commentRepo = commentRepo;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var comments = await _commentRepo.GetAllAsync();
+        var CommentDto = comments.Select(s => s.ToCommentDto());
+        return Ok(CommentDto);
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByIdAsync(int id)
+    {
+        var comment = await _commentRepo.GetByIdAsync(id);
+        if (comment == null) return NotFound();
+        return Ok(comment.ToCommentDto());
+    }
+}
