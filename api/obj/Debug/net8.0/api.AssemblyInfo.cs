9a5850baad91f84db8cd653eb4fa2561bb1d3fc5
omen <paulk198@hotmail.com>
2024-08-02 18:29:53 +0200

    add git ingore

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8324fc2eedb147375bc4ddbd14f1efdec022ecd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25ecf03864ed8956f0fc8c3a11a2f029813844ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
